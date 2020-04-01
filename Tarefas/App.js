c40b09447a8809d5498f3f09b688ba4e647c8cb8
Adiel Cardoso <adieldeveloper@gmail.com>
2020-03-31 23:28:20 -0300

    Criação da tela Splash/Icon Criação do app
    Erro no caminho do index para TaskList

--- Tarefas/App.js
+++ Tarefas/App.js
@@ -1,19 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, TouchableOpacity, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import TaskList from './src/components/TaskList/index';
+
 
 export default function App() {
+  const [task, setTask] = useState([
+    { key:1, task: 'Comprar pão'},
+    { key:2, task: 'estudar RN'},
+    { key:3, task: 'acordar de manha para comer'},
+    { key:4, task: 'carro novo'},
+    { key:5, task: 'note novo'},
+
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor='#20201d' barStyle='light-content' />
+
+      <View style={styles.content}>
+        <Text style={styles.title} >TAREFAS.COM</Text>
+      </View>
+
+      <FlatList 
+      showsVerticalScrollIndicator={false}
+      data={task}
+      keyExtractor={(item)=> String(item.key)}
+      renderItem={({item})=> <TaskList data={item}/>}
+
+      />
+
+      <TouchableOpacity style={styles.fab} >
+        <Ionicons name='ios-add' size={35} color='#fff' />
+      </TouchableOpacity>
+
+
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#980000',
+    
   },
+  content:{
+
+  },
+  title:{
+    marginTop:30,
+    textAlign:'center',
+    fontSize:40,
+    color:'#fff',
+    fontWeight:'bold'
+  },
+  fab:{
+    position:'absolute',
+    width:60,
+    height:60,
+    backgroundColor:'#ffe000',
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:30,
+    right:25,
+    bottom:25,
+    elevation:2,
+    zIndex:9,
+    shadowColor:'#000',
+    shadowOpacity:0.2,
+    shadowOffset:{
+      width:1,
+      height:3,
+    }
+  }
 });
